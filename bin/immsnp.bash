ab43a6779e179bb8223ec16a0135cce8b418d611
Friederike Dündar <frd2007@med.cornell.edu>
2016-10-28 15:38:59 -0400

    pasting stuff from test.sh into pipeline script
    very rough at the moment! won't work!

--- bin/immsnp.bash
+++ bin/immsnp.bash
@@ -72,7 +72,22 @@ until [ -z "$1" ]; do
 done 
 
 
+INPUT_VCF=$1
 
-(source activate python3 && generate_fasta.py --input=${BAM} --output=pvacseq_table${$BAM} --peptide_sequence_length=21)
+FASTA_OUTPUT=file_$RANDOM
+FRED_PREDICTION=fred_${RANDOM}_variant_immunogenicity.csv 
+
+# Definitions for the Docker image  
+SRC_FOLDER=/home/linuxbrew/Cancer_Epitopes_CSHL/src
 
 
+# Use python3 env managed by conda  
+
+source /opt/conda/bin/activate python3 
+python /home/linuxbrew/Cancer_Epitopes_CSHL/src/generate_fasta.py   --input=$INPUT_VCF  --output=$FASTA_OUTPUT --peptide_sequence_length=21
+
+echo "file $FASTA_OUTPUT generated"  
+
+/home/linuxbrew/Cancer_Epitopes_CSHL/src/imm_predict/fred2_allele_prediction.py --input=$FASTA_OUTPUT  --output=${FRED_PREDICTION}
+
+echo "file $FRED_PREDICTION has been written" 
